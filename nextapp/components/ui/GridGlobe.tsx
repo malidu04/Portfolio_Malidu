f9b69157bc7f6a61aa4634a0a40a108db3a40509
Malidu Pahasara <malidupahasara04@gmail.com>
2024-09-14 00:17:33 +0530

    changes_2

--- nextapp/components/ui/GridGlobe.tsx
+++ nextapp/components/ui/GridGlobe.tsx
@@ -7,7 +7,7 @@ const World = dynamic(() => import("./Globe").then((m) => m.World), {
   ssr: false,
 });
 
-export function GlobeDemo() {
+const GridGlobe = () => {
   const globeConfig = {
     pointSize: 4,
     globeColor: "#062056",
@@ -395,9 +395,13 @@ export function GlobeDemo() {
   ];
 
   return (
-    <div className="flex flex-row items-center justify-center py-20 h-screen md:h-auto dark:bg-black bg-white relative w-full">
-      <div className="max-w-7xl mx-auto w-full relative overflow-hidden h-full md:h-[40rem] px-4">
-        <motion.div
+    // remove dark:bg-black bg-white h-screen md:h-auto  w-full flex-row py-20
+    // change absolute -left-5 top-36, add w-full h-full md:top-40
+    <div className="flex items-center justify-center absolute -left-5 top-36 md:top-40 w-full h-full">
+      {/* remove h-full md:h-[40rem] */}
+      <div className="max-w-7xl mx-auto w-full relative overflow-hidden h-96 px-4">
+        {/* remove these text divs */}
+        {/* <motion.div
           initial={{
             opacity: 0,
             y: 20,
@@ -416,14 +420,16 @@ export function GlobeDemo() {
           </h2>
           <p className="text-center text-base md:text-lg font-normal text-neutral-700 dark:text-neutral-200 max-w-md mt-2 mx-auto">
             This globe is interactive and customizable. Have fun with it, and
-            don&apos;t forget to share it. :)
+            don&apos;t forget to share it.
           </p>
-        </motion.div>
+        </motion.div> */}
         <div className="absolute w-full bottom-0 inset-x-0 h-40 bg-gradient-to-b pointer-events-none select-none from-transparent dark:to-black to-white z-40" />
-        <div className="absolute w-full -bottom-20 h-72 md:h-full z-10">
+        {/* remove -bottom-20 */}
+        <div className="absolute w-full h-72 md:h-full z-10">
           <World data={sampleArcs} globeConfig={globeConfig} />
         </div>
       </div>
     </div>
   );
-}
+};
+export default GridGlobe;
