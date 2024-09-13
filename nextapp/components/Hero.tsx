b00467913a97d1fdad388293fc7bbb745eab786a
Malidu Pahasara <malidupahasara04@gmail.com>
2024-09-13 21:56:35 +0530

    project part 2

--- nextapp/components/Hero.tsx
+++ nextapp/components/Hero.tsx
@@ -1,3 +1,5 @@
+import { FaLocationArrow } from 'react-icons/fa';
+import MagicButton from './ui/MagicButton';
 import { Spotlight } from './ui/Spotlight';
 import { TextGenerateEffect } from './ui/TextGenerateEffect';
 
@@ -29,8 +31,16 @@ const Hero = () => {
           />
 
           <p className='text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl'>
-            Hi, I&apos;m Malidu Pahasara, A Sodtware Engineer based on Colombo.
+            Hi, I&apos;m Malidu Pahasara, A Software Engineer based on Colombo.
           </p>
+
+          <a href='#about'>
+            <MagicButton
+              title="Show My Work" 
+              icon={<FaLocationArrow />}
+              position='right'
+              />
+          </a>
         </div>
       </div>
     </div>
--- nextapp/components/ui/TextGenerateEffect.tsx
+++ nextapp/components/ui/TextGenerateEffect.tsx
@@ -49,7 +49,7 @@ export const TextGenerateEffect = ({
 
   return (
     <div className={cn("font-bold", className)}>
-      <div className="mt-4">
+      <div className="my-4">
         <div className=" dark:text-white text-black text-2xl leading-snug tracking-wide">
           {renderWords()}
         </div>
